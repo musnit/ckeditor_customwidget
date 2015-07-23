13d9b29733dea86bcd989a02df87e5642052e60c
Aidan Musnitzky <musnit@gmail.com>
2015-07-23 17:39:36 +0700

    First commit

---
+++ customwidgetlib.js
@@ -0,0 +1,5 @@
+CustomWidget = {};
+
+$( document ).ready(function() {
+});
+
---
+++ plugin.js
@@ -0,0 +1,71 @@
+// Register the plugin within the editor.
+CKEDITOR.plugins.add( 'customwidget', {
+  // This plugin requires the Widgets System defined in the 'widget' plugin.
+  requires: 'widget',
+
+  // Register the icon used for the toolbar button. It must be the same
+  // as the name of the widget.
+  icons: 'customwidget',
+
+  // The plugin initialization logic goes inside this method.
+  init: function( editor ) {
+    // Register the shoppingcart widget.
+    editor.widgets.add( 'customwidget', {
+      allowedContent:
+          'div(!custom-widget);',
+
+      requiredContent: 'div(custom-widget)',
+
+      template: '<div class="custom-widget"></div>',
+
+      upcast: function( element ) {
+        return element.name == 'div' && element.hasClass( 'custom-widget' );
+      },
+      init: function(){
+
+      },
+      data: function(){
+
+      },
+      dialog: 'custom-widget'
+    });
+
+    editor.ui.addButton( 'CustomWidget', {
+
+      // The text part of the button (if available) and tooptip.
+      label: 'Insert Custom Widget',
+
+      // The command to execute on click.
+      command: 'customwidget',
+
+      // The button placement in the toolbar (toolbar group name).
+      toolbar: 'insert'
+    });
+
+    CKEDITOR.dialog.add( 'customwidget', this.path + 'dialogs/customwidget.js' );
+
+    if (editor.addMenuItem) {
+      editor.addMenuGroup('customwidget');
+
+      editor.addMenuItem( 'editWidget', {
+        label: 'Edit Code/HTML',
+        command: 'customwidget',
+        group: 'customwidget'
+      });
+    }
+
+    if (editor.contextMenu) {
+      editor.contextMenu.addListener(function(element, selection) {
+        if (element && element.$ && element.$.firstChild && element.$.firstChild.getAttribute && element.$.firstChild.getAttribute('data-widget') === 'customwidget'){
+          return {
+            editWidget: CKEDITOR.TRISTATE_ON,
+          };
+        }
+        else{
+          return null;
+        }
+      });
+    }
+
+  }
+} );
